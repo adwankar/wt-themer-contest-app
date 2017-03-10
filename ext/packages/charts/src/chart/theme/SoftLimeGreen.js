80e3dc222a8fed3272901a24455738d934c76190
Ranjit Battewad <ranjit.n.battewad@walkingtree.tech>
2017-03-11 00:01:37 +0530

    Added ext lib files

---
+++ ext/packages/charts/src/chart/theme/SoftLimeGreen.js
@@ -0,0 +1,11 @@
+Ext.define('Ext.chart.theme.SoftLimeGreen', {
+    extend: 'Ext.chart.theme.Base',
+    singleton: true,
+    alias: [
+        'chart.theme.softlimegreen',
+        'chart.theme.SoftLimeGreen'
+    ],
+    config: {
+        baseColor: '#61de7a'
+    }
+});
